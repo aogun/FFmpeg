775fc0883b80780faa150a927c42b5650dd49a96
aogun <aogun.china@gmail.com>
2021-07-28 10:10:19 +0800

    add rtmp hevc support 3

--- libavformat/flvdec.c
+++ libavformat/flvdec.c
@@ -370,7 +370,7 @@ static int flv_set_video_codec(AVFormatContext *s, AVStream *vstream,
         break;
     case FLV_CODECID_HEVC:
         par->codec_id = AV_CODEC_ID_HEVC;
-        vstream->need_parsing = AVSTREAM_PARSE_HEADERS;
+        vstream->internal->need_parsing = AVSTREAM_PARSE_HEADERS;
         ret = 3;     // not 4, reading packet type will consume one byte
         break;
     case FLV_CODECID_MPEG4:
